50a0dfe317145179d4cb8af19dd2c9994a31e0cc
Fábio <fa2bio.s0@gmail.com>
2022-10-16 11:29:02 -0300

    Adicionando a classe EntidadeNaoEncontrada ao pacote domain.exception

---
+++ Api/src/main/java/com/github/fa2bio/domain/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,11 @@
+package com.github.fa2bio.domain.exception;
+
+public abstract class EntidadeNaoEncontradaException extends NegocioException {
+
+	private static final long serialVersionUID = 1L;
+
+	public EntidadeNaoEncontradaException(String mensagem) {
+		super(mensagem);
+	}
+	
+}
