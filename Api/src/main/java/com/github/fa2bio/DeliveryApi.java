310da4ac03d33f5ff6ed1d8d4e90f3e69ae027e5
Fábio <fa2bio.s0@gmail.com>
2022-10-17 08:49:02 -0300

    Adicionando o metodo main

---
+++ Api/src/main/java/com/github/fa2bio/DeliveryApi.java
@@ -0,0 +1,17 @@
+package com.github.fa2bio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.github.fa2bio.infrastructure.repository.CustomJpaRepositoryImpl;
+
+@SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
+public class DeliveryApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryApi.class, args);
+	}
+
+}
