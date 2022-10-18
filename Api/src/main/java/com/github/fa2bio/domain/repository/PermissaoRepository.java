ca5b92404503b491a068915fcd59f558c0f5a25b
Fábio <fa2bio.s0@gmail.com>
2022-10-18 10:15:28 -0300

    Adicionando a classe PermissaoRepository ao pacote domain.repository

---
+++ Api/src/main/java/com/github/fa2bio/domain/repository/PermissaoRepository.java
@@ -0,0 +1,11 @@
+package com.github.fa2bio.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.github.fa2bio.domain.model.Permissao;
+
+@Repository
+public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
+
+}
