aecfe4e0c32e0370fea10a6482a5ad6d4cb65e80
Fábio <fa2bio.s0@gmail.com>
2022-10-19 10:05:56 -0300

    Adicionando a classe RestauranteSpecs ao pacote repository.spec

---
+++ Api/src/main/java/com/github/fa2bio/infrastructure/repository/spec/RestauranteSpecs.java
@@ -0,0 +1,21 @@
+package com.github.fa2bio.infrastructure.repository.spec;
+
+import java.math.BigDecimal;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.github.fa2bio.domain.model.Restaurante;
+
+public class RestauranteSpecs {
+
+	public static Specification<Restaurante> comFreteGratis() {
+		return (root, query, builder) -> 
+			builder.equal(root.get("taxaFrete"), BigDecimal.ZERO);
+	}
+	
+	public static Specification<Restaurante> comNomeSemelhante(String nome) {
+		return (root, query, builder) ->
+			builder.like(root.get("nome"), "%" + nome + "%");
+	}
+	
+}
