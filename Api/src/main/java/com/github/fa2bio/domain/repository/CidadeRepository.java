40a8b6bad8a58713c5e711363feb38a0a05d8c87
Fábio <fa2bio.s0@gmail.com>
2022-10-21 12:39:56 -0300

    Editando a classe CidadeRepository para resolver o problema do n+1 do hibernate

--- Api/src/main/java/com/github/fa2bio/domain/repository/CidadeRepository.java
+++ Api/src/main/java/com/github/fa2bio/domain/repository/CidadeRepository.java
@@ -1,6 +1,9 @@
 package com.github.fa2bio.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.github.fa2bio.domain.model.Cidade;
@@ -8,4 +11,6 @@ import com.github.fa2bio.domain.model.Cidade;
 @Repository
 public interface CidadeRepository extends JpaRepository<Cidade, Long> {
 
+	@Query("from Cidade r join fetch r.estado")
+	List<Cidade> findAll();
 }
