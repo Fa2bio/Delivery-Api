b6b9d68109f4778d02c420faf1525cfe44a0d917
Fábio <fa2bio.s0@gmail.com>
2022-10-21 00:44:24 -0300

    Alterando a classe RestauranteController para aceitar o padrão DTO

--- Api/src/main/java/com/github/fa2bio/api/controller/RestauranteController.java
+++ Api/src/main/java/com/github/fa2bio/api/controller/RestauranteController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.fa2bio.api.assembler.RestauranteModelAssembler;
+import com.github.fa2bio.api.assembler.RestauranteModelDisassembler;
+import com.github.fa2bio.api.model.RestauranteModel;
+import com.github.fa2bio.api.model.input.RestauranteInput;
 import com.github.fa2bio.domain.exception.CozinhaNaoEncontradaException;
 import com.github.fa2bio.domain.exception.NegocioException;
 import com.github.fa2bio.domain.model.Restaurante;
@@ -31,41 +34,52 @@ public class RestauranteController {
 	
 	@Autowired
 	private CadastroRestauranteService cadastroRestaurante;
+	
+	@Autowired
+	private RestauranteModelAssembler restauranteModelAssembler;
+	
+	@Autowired
+	private RestauranteModelDisassembler restauranteModelDisassembler;
 
 	@GetMapping
-	public List<Restaurante> listar() {
-		return restauranteRepository.findAll();
+	public List<RestauranteModel> listar() {
+		return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
 	}
 	
 	@GetMapping("/{restauranteId}")
-	public Restaurante buscar(@PathVariable Long restauranteId) {
-		return cadastroRestaurante.buscarOuFalhar(restauranteId);
+	public RestauranteModel buscar(@PathVariable Long restauranteId) {
+		Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
+		return restauranteModelAssembler.toModel(restaurante);
 	}
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Restaurante adicionar(@RequestBody @Valid Restaurante restaurante) {
+	public RestauranteModel adicionar(@RequestBody @Valid RestauranteInput restauranteInput) {
 
 		try {
-			return cadastroRestaurante.salvar(restaurante);
+			Restaurante restaurante = restauranteModelDisassembler.toDomainObject(restauranteInput);
+			return restauranteModelAssembler.toModel(cadastroRestaurante.salvar(restaurante));
 		} catch (CozinhaNaoEncontradaException e) {
 			throw new NegocioException(e.getMessage());
 		}
 	}
 	
 	@PutMapping("/{restauranteId}")
-	public Restaurante atualizar(@PathVariable Long restauranteId,
-			@RequestBody @Valid Restaurante restaurante) {
+	public RestauranteModel atualizar(@PathVariable Long restauranteId,
+			@RequestBody @Valid RestauranteInput restauranteInput) {
 		try {
+			//Restaurante restaurante = restauranteModelDisassembler.toDomainObject(restauranteInput);
+			
 			Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
 			
-			BeanUtils.copyProperties(restaurante, restauranteAtual, 
-					"id", "formasPagamento", "endereco", "dataCadastro", "produtos");
+			restauranteModelDisassembler.copyToDomainObject(restauranteInput, restauranteAtual);
+			//BeanUtils.copyProperties(restaurante, restauranteAtual, 
+					//"id", "formasPagamento", "endereco", "dataCadastro", "produtos");
 
-			return cadastroRestaurante.salvar(restauranteAtual);
+			return restauranteModelAssembler.toModel(cadastroRestaurante.salvar(restauranteAtual));
 		} catch (CozinhaNaoEncontradaException e) {
 			throw new NegocioException(e.getMessage());
 		}
 	}
-	
+
 }
