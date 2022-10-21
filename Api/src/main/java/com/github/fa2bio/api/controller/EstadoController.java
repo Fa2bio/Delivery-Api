154f3fc82e385f48dbc41985bb36ce535b6aa741
Fábio <fa2bio.s0@gmail.com>
2022-10-21 12:40:27 -0300

    Atualizando a classe EstadoController para o padrão DTO

--- Api/src/main/java/com/github/fa2bio/api/controller/EstadoController.java
+++ Api/src/main/java/com/github/fa2bio/api/controller/EstadoController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.fa2bio.api.assembler.EstadoModelAssembler;
+import com.github.fa2bio.api.assembler.EstadoModelDisassembler;
+import com.github.fa2bio.api.model.EstadoModel;
+import com.github.fa2bio.api.model.input.EstadoInput;
 import com.github.fa2bio.domain.model.Estado;
 import com.github.fa2bio.domain.repository.EstadoRepository;
 import com.github.fa2bio.domain.service.CadastroEstadoService;
@@ -31,30 +34,38 @@ public class EstadoController {
 	@Autowired
 	private CadastroEstadoService cadastroEstado;
 	
+	@Autowired
+	private EstadoModelAssembler estadoModelAssembler;
+	
+	@Autowired
+	private EstadoModelDisassembler estadoModelDisassembler;
+	
 	@GetMapping
-	public List<Estado> listar() {
-		return estadoRepository.findAll();
+	public List<EstadoModel> listar() {
+		return estadoModelAssembler.toCollectionModel(estadoRepository.findAll());
 	}
 	
 	@GetMapping("/{estadoId}")
-	public Estado buscar(@PathVariable Long estadoId) {
-		return cadastroEstado.buscarOuFalhar(estadoId);
+	public EstadoModel buscar(@PathVariable Long estadoId) {
+		Estado estado = cadastroEstado.buscarOuFalhar(estadoId);
+		return estadoModelAssembler.toModel(estado);
 	}
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Estado adicionar(@RequestBody @Valid Estado estado) {
-		return cadastroEstado.salvar(estado);
+	public EstadoModel adicionar(@RequestBody @Valid EstadoInput estadoInput) {
+		Estado estado = estadoModelDisassembler.toDomainObject(estadoInput);
+		return estadoModelAssembler.toModel(cadastroEstado.salvar(estado));
 	}
 	
 	@PutMapping("/{estadoId}")
-	public Estado atualizar(@PathVariable Long estadoId,
-			@RequestBody @Valid Estado estado) {
+	public EstadoModel atualizar(@PathVariable Long estadoId,
+			@RequestBody @Valid EstadoInput estadoInput) {
 		Estado estadoAtual = cadastroEstado.buscarOuFalhar(estadoId);
 		
-		BeanUtils.copyProperties(estado, estadoAtual, "id");
+		estadoModelDisassembler.copyToDomainObject(estadoInput, estadoAtual);
 		
-		return cadastroEstado.salvar(estadoAtual);
+		return estadoModelAssembler.toModel(cadastroEstado.salvar(estadoAtual));
 	}
 	
 	@DeleteMapping("/{estadoId}")
