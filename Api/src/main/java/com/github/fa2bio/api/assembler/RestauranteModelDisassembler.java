664522f9b213e8e05ecf56ad93033c4c22e60db2
Fábio <fa2bio.s0@gmail.com>
2022-10-21 00:45:57 -0300

    Adicionando o padrão DTO ao projeto

---
+++ Api/src/main/java/com/github/fa2bio/api/assembler/RestauranteModelDisassembler.java
@@ -0,0 +1,28 @@
+package com.github.fa2bio.api.assembler;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.github.fa2bio.api.model.input.RestauranteInput;
+import com.github.fa2bio.domain.model.Cozinha;
+import com.github.fa2bio.domain.model.Restaurante;
+
+@Component
+public class RestauranteModelDisassembler {
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	public Restaurante toDomainObject(RestauranteInput restauranteInput) {
+		return modelMapper.map(restauranteInput, Restaurante.class);
+	}
+	
+	public void copyToDomainObject(RestauranteInput restauranteInput, Restaurante restaurante) {
+		//Para evitar org.hibernate.HibernateException: identifier of an instance 
+		//of com.github.fa2bio.domain.model.Cozinha was altered from 1 to 2
+		restaurante.setCozinha(new Cozinha());
+		
+		modelMapper.map(restauranteInput, restaurante);
+	}
+}
