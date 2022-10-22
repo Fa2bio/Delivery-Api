2f40d5e3aa407bac9acadc60c1e7ab4fcc4db542
Fábio <fa2bio.s0@gmail.com>
2022-10-22 01:06:16 -0300

    Adicionando os Assemblers da classe Cidade

---
+++ Api/src/main/java/com/github/fa2bio/api/assembler/CidadeInputDisassembler.java
@@ -0,0 +1,27 @@
+package com.github.fa2bio.api.assembler;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.github.fa2bio.api.model.input.CidadeInput;
+import com.github.fa2bio.domain.model.Cidade;
+import com.github.fa2bio.domain.model.Estado;
+
+@Component
+public class CidadeInputDisassembler {
+
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	public Cidade toDomainObject(CidadeInput cidadeInput) {
+		return modelMapper.map(cidadeInput, Cidade.class);
+	}
+	
+	public void copyToDomainObject(CidadeInput cidadeInput, Cidade cidade) {
+		//Para evitar org.hibernate.HibernateException: identifier of an instance 
+		//of com.github.fa2bio.domain.model.Cidade was altered from 1 to 2
+		cidade.setEstado(new Estado());
+		modelMapper.map(cidadeInput, cidade);
+	}
+}
