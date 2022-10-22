924711d0bd537992bc8d5825916b9a3fbcc825c4
Fábio <fa2bio.s0@gmail.com>
2022-10-22 01:07:39 -0300

    Adicionando os Assemblers da classe Grupo

---
+++ Api/src/main/java/com/github/fa2bio/api/assembler/GrupoInputDisassembler.java
@@ -0,0 +1,22 @@
+package com.github.fa2bio.api.assembler;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.github.fa2bio.api.model.input.GrupoInput;
+import com.github.fa2bio.domain.model.Grupo;
+
+@Component
+public class GrupoInputDisassembler {
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	public Grupo toDomainObject(GrupoInput grupoInput) {
+		return modelMapper.map(grupoInput, Grupo.class);
+	}
+	
+	public void copyToDomainObject(GrupoInput grupoInput, Grupo grupo) {
+		modelMapper.map(grupoInput, grupo);
+	}
+}
---
+++ Api/src/main/java/com/github/fa2bio/api/assembler/GrupoModelAssembler.java
@@ -0,0 +1,27 @@
+package com.github.fa2bio.api.assembler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.github.fa2bio.api.model.GrupoModel;
+import com.github.fa2bio.domain.model.Grupo;
+
+@Component
+public class GrupoModelAssembler {
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	public GrupoModel toModel(Grupo grupo) {
+		return modelMapper.map(grupo, GrupoModel.class);
+	}
+	
+	public List<GrupoModel> toCollectionModel(List<Grupo> grupos){
+		return grupos.stream()
+				.map(grupo -> toModel(grupo))
+				.collect(Collectors.toList());
+	}
+}
