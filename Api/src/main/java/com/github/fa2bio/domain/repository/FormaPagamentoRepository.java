4185a6132d839837fd22657d1b1de8b4a67bdbec
Fábio <fa2bio.s0@gmail.com>
2022-10-23 20:27:11 -0300

    Atualizando as classes de FormaPagamento

--- Api/src/main/java/com/github/fa2bio/domain/repository/FormaPagamentoRepository.java
+++ Api/src/main/java/com/github/fa2bio/domain/repository/FormaPagamentoRepository.java
@@ -1,11 +1,10 @@
 package com.github.fa2bio.domain.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.github.fa2bio.domain.model.FormaPagamento;
 
 @Repository
-public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
+public interface FormaPagamentoRepository extends CustomJpaRepository<FormaPagamento, Long> {
 
 }
