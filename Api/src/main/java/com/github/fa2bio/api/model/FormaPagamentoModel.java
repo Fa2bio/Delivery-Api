42db9be41b4a923e418bc316a1b905e6218ca4c2
Fábio <fa2bio.s0@gmail.com>
2022-10-23 20:34:58 -0300

    Adicionando DTO da classe FormaPagamento

---
+++ Api/src/main/java/com/github/fa2bio/api/model/FormaPagamentoModel.java
@@ -0,0 +1,12 @@
+package com.github.fa2bio.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FormaPagamentoModel {
+
+	private Long id;
+	private String descricao;
+}
---
+++ Api/src/main/java/com/github/fa2bio/api/model/input/FormaPagamentoInput.java
@@ -0,0 +1,14 @@
+package com.github.fa2bio.api.model.input;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FormaPagamentoInput {
+	
+	@NotBlank
+	private String descricao;
+}
