85840347813fc6fee1f1a2be7222b9a1dff194b0
Fábio <fa2bio.s0@gmail.com>
2022-10-23 20:28:54 -0300

    Atualizando as heranças das classes Repository

--- Api/src/main/java/com/github/fa2bio/domain/repository/CidadeRepository.java
+++ Api/src/main/java/com/github/fa2bio/domain/repository/CidadeRepository.java
@@ -2,14 +2,13 @@ package com.github.fa2bio.domain.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.github.fa2bio.domain.model.Cidade;
 
 @Repository
-public interface CidadeRepository extends JpaRepository<Cidade, Long> {
+public interface CidadeRepository extends CustomJpaRepository<Cidade, Long> {
 
 	@Query("from Cidade r join fetch r.estado")
 	List<Cidade> findAll();
