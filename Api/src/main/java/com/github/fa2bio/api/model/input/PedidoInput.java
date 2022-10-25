4487ca6f31b2854a146f168e0508bc9ef9aec8eb
Fábio <fa2bio.s0@gmail.com>
2022-10-25 13:38:26 -0300

    Update PedidoInput.java

--- Api/src/main/java/com/github/fa2bio/api/model/input/PedidoInput.java
+++ Api/src/main/java/com/github/fa2bio/api/model/input/PedidoInput.java
@@ -1,20 +1,10 @@
 package com.github.fa2bio.api.model.input;
 
-import java.math.BigDecimal;
-import java.time.OffsetDateTime;
 import java.util.List;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
-
-import com.github.fa2bio.api.model.EnderecoModel;
-import com.github.fa2bio.api.model.FormaPagamentoModel;
-import com.github.fa2bio.api.model.ItemPedidoModel;
-import com.github.fa2bio.api.model.RestauranteResumoModel;
-import com.github.fa2bio.api.model.UsuarioModel;
-import com.github.fa2bio.domain.model.Endereco;
-import com.github.fa2bio.domain.model.StatusPedido;
+import javax.validation.constraints.Size;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -23,54 +13,20 @@ import lombok.Setter;
 @Setter
 public class PedidoInput {
 	
+	@Valid
 	@NotNull
-	private Long id;
-	
-	@NotNull
-	@PositiveOrZero
-	private BigDecimal subtotal;
-	
-	@NotNull
-	@PositiveOrZero
-	private BigDecimal taxaFrete;
-	
-	@NotNull
-	@PositiveOrZero
-	private BigDecimal valorTotal;
-	
-	@NotNull
-	@PositiveOrZero
-	private Endereco enderecoEntrega;
-	
-	@NotNull
-	private StatusPedido status = StatusPedido.CRIADO;
-	
-	@NotNull
-	private OffsetDateTime dataConfirmacao;
-	
-	@NotNull
-	private OffsetDateTime dataCancelamento;
-	
-	@NotNull
-	private OffsetDateTime dataEntrega;
+	private RestauranteIdInput restauranteIdInput;
 	
 	@Valid
 	@NotNull
-	private RestauranteResumoModel restauranteResumoModel;
+	private FormaPagamentoIdInput formaPagamentoIdInput;
 	
 	@Valid
 	@NotNull
-	private UsuarioModel usuarioModel;
+	private EnderecoInput enderecoInput;
 	
 	@Valid
 	@NotNull
-	private FormaPagamentoModel formaPagamentoModel;
-	
-	@Valid
-	@NotNull
-	private EnderecoModel enderecoModel;
-	
-	@Valid
-	@NotNull
-	private List<ItemPedidoModel> itens;  
+	@Size(min=1)
+	private List<ItemPedidoInput> itens;  
 }
