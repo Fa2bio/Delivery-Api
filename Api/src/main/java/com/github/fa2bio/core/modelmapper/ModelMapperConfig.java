388d14689506003bcb694805e40ae1dfcf64485f
Fábio <fa2bio.s0@gmail.com>
2022-10-25 13:38:56 -0300

    Update ModelMapperConfig.java

--- Api/src/main/java/com/github/fa2bio/core/modelmapper/ModelMapperConfig.java
+++ Api/src/main/java/com/github/fa2bio/core/modelmapper/ModelMapperConfig.java
@@ -4,16 +4,31 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//import com.github.fa2bio.api.model.UsuarioModel;
-//import com.github.fa2bio.domain.model.Usuario;
+//import com.github.fa2bio.api.model.EnderecoModel;
+//import com.github.fa2bio.api.model.RestauranteModel;
+//import com.github.fa2bio.domain.model.Endereco;
+//import com.github.fa2bio.domain.model.Restaurante;
 
+import com.github.fa2bio.api.model.input.ItemPedidoInput;
+import com.github.fa2bio.domain.model.ItemPedido;
 @Configuration
 public class ModelMapperConfig {
 	@Bean
 	public ModelMapper modelMapper() {
 		var modelMapper = new ModelMapper();
-//		var usuarioToUsuarioModelTypeMap = modelMapper
-//			.createTypeMap(Usuario.class, UsuarioModel.class);
+		
+//		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+//			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setTaxaFrete);
+		
+//		var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoModel.class);
+//		
+//		enderecoToEnderecoModelTypeMap.<String>addMapping
+//				(enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+//				(enderecoModelDest, value) -> enderecoModelDest.getCidade().setNomeEstado(value));
+		
+		modelMapper.createTypeMap(ItemPedidoInput.class, ItemPedido.class)
+		.addMappings(mapper -> mapper.skip(ItemPedido::setId));
 		return modelMapper;
 	}
 }
+
