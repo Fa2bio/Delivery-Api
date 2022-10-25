6d8089f1d212a1ffd6bd33b3f5ea11ff5ac8be1a
Fábio <fa2bio.s0@gmail.com>
2022-10-25 13:37:46 -0300

    Update Cidade.java

--- Api/src/main/java/com/github/fa2bio/domain/model/Cidade.java
+++ Api/src/main/java/com/github/fa2bio/domain/model/Cidade.java
@@ -7,9 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 
@@ -28,12 +25,9 @@ public class Cidade {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 	
-	@Valid
-	@NotNull
 	@ManyToOne
 	@JoinColumn(nullable = false)
 	@ConvertGroup(from = Default.class, to = Groups.EstadoId.class)
