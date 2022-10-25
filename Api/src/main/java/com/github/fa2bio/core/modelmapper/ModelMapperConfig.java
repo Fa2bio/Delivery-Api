e81ab1d21fb6a7a45a382be871216b0012f118ee
Fábio <fa2bio.s0@gmail.com>
2022-10-25 12:01:52 -0300

    Update ModelMapperConfig.java

--- Api/src/main/java/com/github/fa2bio/core/modelmapper/ModelMapperConfig.java
+++ Api/src/main/java/com/github/fa2bio/core/modelmapper/ModelMapperConfig.java
@@ -4,16 +4,16 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//import com.github.fa2bio.api.model.UsuarioModel;
+//import com.github.fa2bio.domain.model.Usuario;
+
 @Configuration
 public class ModelMapperConfig {
-
 	@Bean
 	public ModelMapper modelMapper() {
 		var modelMapper = new ModelMapper();
-		
-		//var usuarioToUsuarioModelTypeMap = modelMapper.createTypeMap(Usuario.class, UsuarioModel.class);
-
-		
+//		var usuarioToUsuarioModelTypeMap = modelMapper
+//			.createTypeMap(Usuario.class, UsuarioModel.class);
 		return modelMapper;
 	}
 }
