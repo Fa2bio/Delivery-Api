a28337276cbafcb137458b36e1938c5199bfba9e
Fábio <fa2bio.s0@gmail.com>
2022-10-24 21:50:54 -0300

    Adicionando migração V009 ao Flyway

--- Api/src/main/resources/db/testdata/afterMigrate.sql
+++ Api/src/main/resources/db/testdata/afterMigrate.sql
@@ -8,8 +8,11 @@ delete from grupo;
 delete from grupo_permissao;
 delete from permissao;
 delete from produto;
+delete from pedido;
+delete from item_pedido;
 delete from restaurante;
 delete from restaurante_forma_pagamento;
+delete from restaurante_usuario;
 delete from usuario;
 delete from usuario_grupo;
 
@@ -22,6 +25,7 @@ alter table forma_pagamento auto_increment = 1;
 alter table grupo auto_increment = 1;
 alter table permissao auto_increment = 1;
 alter table produto auto_increment = 1;
+alter table pedido auto_increment = 1;
 alter table restaurante auto_increment = 1;
 alter table usuario auto_increment = 1;
 
@@ -40,12 +44,12 @@ insert into cidade (id, nome, estado_id) values (3, 'São Paulo', 2);
 insert into cidade (id, nome, estado_id) values (4, 'Campinas', 2);
 insert into cidade (id, nome, estado_id) values (5, 'Fortaleza', 3);
 
-insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values (1, 'Burguer King', 10, 1, utc_timestamp, utc_timestamp, true, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
-insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo) values (2, 'Mc Donalds', 9.50, 1, utc_timestamp, utc_timestamp, true);
-insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo) values (3, 'Girafas', 15, 2, utc_timestamp, utc_timestamp, true);
-insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo) values (4, 'Habibs', 12, 3, utc_timestamp, utc_timestamp, true);
-insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo) values (5, 'Caravana Lanches', 11, 4, utc_timestamp, utc_timestamp, true);
-insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo) values (6, 'Bar do zé', 6, 4, utc_timestamp, utc_timestamp, true);
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values (1, 'Burguer King', 10, 1, utc_timestamp, utc_timestamp, true, true, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto) values (2, 'Mc Donalds', 9.50, 1, utc_timestamp, utc_timestamp, true, true);
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto) values (3, 'Girafas', 15, 2, utc_timestamp, utc_timestamp, true, true);
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto) values (4, 'Habibs', 12, 3, utc_timestamp, utc_timestamp, true, true);
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto) values (5, 'Caravana Lanches', 11, 4, utc_timestamp, utc_timestamp, true, true);
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto) values (6, 'Bar do zé', 6, 4, utc_timestamp, utc_timestamp, true, true);
 
 insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
 insert into forma_pagamento (id, descricao) values (2, 'Cartão de débito');
@@ -54,18 +58,16 @@ insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
 insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
 insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
 
-insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3), (4, 1), (4, 2), (5, 1), (5, 2), (6, 3);
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Whopper', 'Delicioso hamburguer com carne bovina e salda', 28.90, 1, 1);
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Big King', 'Delicioso haburguer duplo com carne bovina e molho especial', 25.9, 1, 1);
 
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Whopper', 'Delicioso hamburguer com carne bovina e salda', 78.90, 1, 1);
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Big King', 'Delicioso haburguer duplo com carne bovina e molho especial', 110, 1, 1);
-
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Big Mac', 'Delicioso haburguer duplo com carne bovina e molho especial Big Mac', 87.20, 1, 2);
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Big Mac', 'Delicioso haburguer duplo com carne bovina e molho especial Big Mac', 37.20, 1, 2);
 
 insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Brasileirinho', 'Arroz, feijão e file à cavalo', 21, 1, 3);
 insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Frango ao molho', 'Cubos de frango preparados com molho curry e especiarias', 43, 1, 3);
 
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Bib esfiha clássica', 'Deliciosa esfiha de carne, calabresa ou queijo', 79, 1, 4);
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Pastel de Belem', 'Delicioso doce portugues feito com a nata do leite', 89, 1, 4);
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Bib esfiha clássica', 'Deliciosa esfiha de carne, calabresa ou queijo', 0.99, 1, 4);
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Pastel de Belem', 'Delicioso doce portugues feito com a nata do leite', 2.89, 1, 4);
 
 insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Sanduíche X-Tudo', 'Sandubão com muito queijo, hamburger bovino, bacon, ovo, salada e maionese', 19, 1, 5);
 
@@ -76,5 +78,35 @@ insert into grupo (nome) values ('Gerente'), ('Vendedor'), ('Secretária'), ('Ca
 insert into usuario (id, nome, email, senha, data_cadastro) values
 (1, 'Fábio da Silva', 'fabio.ger@test.com', '123', utc_timestamp),
 (2, 'Marta Carvalho', 'marta.vnd@test.com', '456', utc_timestamp),
-(3, 'José Souza', 'jose.aux@test.com', '789', utc_timestamp),
+(3, 'José Souza', 'jose.sec@test.com', '789', utc_timestamp),
 (4, 'Carlos Martins', 'Carlos.cad@test.com', '000', utc_timestamp);
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3), (4, 1), (4, 2), (5, 1), (5, 2), (6, 3);
+
+insert into grupo_permissao (grupo_id, permissao_id) values (1,1), (1,2), (2,1), (3,1), (4,1), (4,2);
+
+insert into usuario_grupo (usuario_id, grupo_id) values (1,1), (2,2), (3,3), (4,4);
+
+insert into restaurante_usuario (restaurante_id, usuario_id) values (1,1), (2,2), (3,3), (4,4), (5,2), (6,3),(1,4);
+
+insert into pedido (id, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep, 
+                    endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro,
+	                status, data_criacao, subtotal, taxa_frete, valor_total)
+values (1, 1, 1, 1, 1, '38400-000', 'Rua Floriano Peixoto', '500', 'Apto 801', 'Brasil',
+        'CRIADO', utc_timestamp, 298.90, 10, 308.90);
+
+insert into item_pedido (id, pedido_id, produto_id, quantidade, preco_unitario, preco_total, observacao)
+values (1, 1, 1, 1, 78.9, 78.9, null);
+
+insert into item_pedido (id, pedido_id, produto_id, quantidade, preco_unitario, preco_total, observacao)
+values (2, 1, 2, 2, 110, 220, 'Com mais queijo, por favor');
+
+
+insert into pedido (id, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep, 
+                    endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro,
+	                status, data_criacao, subtotal, taxa_frete, valor_total)
+values (2, 4, 1, 2, 1, '38400-111', 'Rua Acre', '300', 'Casa 2', 'Centro',
+        'CRIADO', utc_timestamp, 79, 0, 79);
+
+insert into item_pedido (id, pedido_id, produto_id, quantidade, preco_unitario, preco_total, observacao)
+values (3, 2, 6, 1, 79, 79, 'Ao ponto');
