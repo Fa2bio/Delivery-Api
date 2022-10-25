30bcf58041c665d928bc50e3f924dd727602aadd
Fábio <fa2bio.s0@gmail.com>
2022-10-25 13:38:01 -0300

    Update Pedido.java

--- Api/src/main/java/com/github/fa2bio/domain/model/Pedido.java
+++ Api/src/main/java/com/github/fa2bio/domain/model/Pedido.java
@@ -5,6 +5,7 @@ import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -60,7 +61,7 @@ public class Pedido {
 	@JoinColumn(name = "usuario_cliente_id", nullable = false)
 	private Usuario cliente;
 	
-	@OneToMany(mappedBy = "pedido")
+	@OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
 	private List<ItemPedido> itens = new ArrayList<>();
 
 }
