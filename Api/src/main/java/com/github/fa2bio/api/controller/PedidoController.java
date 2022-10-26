7c83fe78169c1629afdfce990d3b3e42ba614b19
Fábio <fa2bio.s0@gmail.com>
2022-10-26 11:20:22 -0300

    Update PedidoController.java

--- Api/src/main/java/com/github/fa2bio/api/controller/PedidoController.java
+++ Api/src/main/java/com/github/fa2bio/api/controller/PedidoController.java
@@ -2,16 +2,24 @@ package com.github.fa2bio.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.fa2bio.api.assembler.PedidoInputDisassembler;
 import com.github.fa2bio.api.assembler.PedidoModelAssembler;
 import com.github.fa2bio.api.assembler.PedidoResumoModelAssembler;
 import com.github.fa2bio.api.model.PedidoModel;
 import com.github.fa2bio.api.model.PedidoResumoModel;
+import com.github.fa2bio.api.model.input.PedidoInput;
 import com.github.fa2bio.domain.model.Pedido;
 import com.github.fa2bio.domain.repository.PedidoRepository;
 import com.github.fa2bio.domain.service.EmissaoPedidoService;
@@ -19,18 +27,21 @@ import com.github.fa2bio.domain.service.EmissaoPedidoService;
 @RestController
 @RequestMapping(value = "/pedidos")
 public class PedidoController {
-
+	
 	@Autowired
 	private PedidoRepository pedidoRepository;
 	
 	@Autowired
-	private EmissaoPedidoService emissaoPedido;
+	private EmissaoPedidoService emissaoPedidoService;
 	
 	@Autowired
 	private PedidoModelAssembler pedidoModelAssembler;
 	
 	@Autowired
 	private PedidoResumoModelAssembler pedidoResumoModelAssembler;
+	
+	@Autowired
+	private PedidoInputDisassembler pedidoInputDisassembler;
 	
 	@GetMapping
 	public List<PedidoResumoModel> listar() {
@@ -41,9 +52,15 @@ public class PedidoController {
 	
 	@GetMapping("/{pedidoId}")
 	public PedidoModel buscar(@PathVariable Long pedidoId) {
-		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
+		Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidoId);
 		
 		return pedidoModelAssembler.toModel(pedido);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public PedidoModel adicionar(@RequestBody @Valid PedidoInput pedidoInput) {
+		Pedido pedido = pedidoInputDisassembler.toDomainObject(pedidoInput);
 	}
 	
 }
