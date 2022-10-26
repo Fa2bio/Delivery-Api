29ac5b1855b811d378019243913179b22046fed3
Fábio <fa2bio.s0@gmail.com>
2022-10-26 11:19:52 -0300

    Create UsuarioGrupoController.java

---
+++ Api/src/main/java/com/github/fa2bio/api/controller/UsuarioGrupoController.java
@@ -0,0 +1,48 @@
+package com.github.fa2bio.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.fa2bio.api.assembler.GrupoModelAssembler;
+import com.github.fa2bio.api.model.GrupoModel;
+import com.github.fa2bio.domain.model.Usuario;
+import com.github.fa2bio.domain.service.CadastroUsuarioService;
+
+@RestController
+@RequestMapping("/usuarios/{usuarioId}")
+public class UsuarioGrupoController {
+
+	@Autowired
+	private CadastroUsuarioService cadastroUsuarioService;
+	
+	@Autowired
+	private GrupoModelAssembler grupoModelAssembler;
+	
+	@GetMapping("/grupos")
+	public List<GrupoModel> listar(@PathVariable Long usuarioId){
+		Usuario usuario = cadastroUsuarioService.buscarOuFalhar(usuarioId);
+		return grupoModelAssembler.toCollectionModel(usuario.getGrupos());
+	}
+	
+	@PutMapping("/grupos/{grupoId}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void associar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
+		cadastroUsuarioService.associar(usuarioId, grupoId);
+	}
+	
+	@DeleteMapping("/grupos/{grupoId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deassociar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
+		cadastroUsuarioService.deassociar(usuarioId, grupoId);
+	}
+	
+}
