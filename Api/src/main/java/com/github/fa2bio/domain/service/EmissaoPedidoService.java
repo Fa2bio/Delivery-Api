689f183a1c50356b7683f787a22ef12d46094a11
Fábio <fa2bio.s0@gmail.com>
2022-10-28 18:15:04 -0300

    Update EmissaoPedidoService.java

--- Api/src/main/java/com/github/fa2bio/domain/service/EmissaoPedidoService.java
+++ Api/src/main/java/com/github/fa2bio/domain/service/EmissaoPedidoService.java
@@ -1,10 +1,20 @@
 package com.github.fa2bio.domain.service;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.github.fa2bio.domain.exception.FormaPagamentoNaoEncontradaException;
 import com.github.fa2bio.domain.exception.PedidoNaoEncontradoException;
+import com.github.fa2bio.domain.model.Cidade;
+import com.github.fa2bio.domain.model.FormaPagamento;
+import com.github.fa2bio.domain.model.ItemPedido;
 import com.github.fa2bio.domain.model.Pedido;
+import com.github.fa2bio.domain.model.Produto;
+import com.github.fa2bio.domain.model.Restaurante;
+import com.github.fa2bio.domain.model.Usuario;
 import com.github.fa2bio.domain.repository.PedidoRepository;
 
 @Service
@@ -13,9 +23,66 @@ public class EmissaoPedidoService {
 	@Autowired
 	private PedidoRepository pedidoRepository;
 	
+	@Autowired
+	private CadastroCidadeService cadastroCidadeService;
+	
+	@Autowired
+	private CadastroUsuarioService cadastroUsuarioService;
+	
+	@Autowired
+	private CadastroRestauranteService cadastroRestauranteService;
+	
+	@Autowired
+	private CadastroFormaPagamentoService cadastroFormaPagamentoService;
+	
+	@Autowired
+	private CadastroProdutoService cadastroProdutoService;
+	
 	public Pedido buscarOuFalhar(Long pedidoId) {
 		return pedidoRepository.findById(pedidoId)
 			.orElseThrow(() -> new PedidoNaoEncontradoException(pedidoId));
+	}
+	
+	@Transactional
+	public Pedido emitir(Pedido pedido) {
+		validarPedido(pedido);
+		validarItens(pedido);		
+		pedido.setTaxaFrete(pedido.getRestaurante().getTaxaFrete());
+		pedido.calcularTotal();
+		
+		return pedidoRepository.save(pedido);
+	}
+
+	private void validarPedido(Pedido pedido) {
+		Restaurante restaurante = cadastroRestauranteService.buscarOuFalhar(pedido.getRestaurante().getId());
+		FormaPagamento formaPagamento = cadastroFormaPagamentoService.buscarOuFalhar(pedido.getFormaPagamento().getId());
+		Cidade cidade = cadastroCidadeService.buscarOuFalhar(pedido.getEnderecoEntrega().getCidade().getId());
+		Usuario cliente = cadastroUsuarioService.buscarOuFalhar(pedido.getCliente().getId());
+		
+		for (ItemPedido itemPedido : pedido.getItens()) {
+			Produto produto = cadastroProdutoService.buscarOuFalhar(restaurante.getId(), itemPedido.getProduto().getId());
+			itemPedido.setProduto(produto);
+		}
+		
+		if(!restaurante.getFormasPagamento().contains(formaPagamento)) throw new FormaPagamentoNaoEncontradaException(formaPagamento.getId(), restaurante.getId());
+		
+		pedido.setRestaurante(restaurante);
+		pedido.setFormaPagamento(formaPagamento);
+		pedido.setCliente(cliente);
+		pedido.getEnderecoEntrega().setCidade(cidade);
+		pedido.getEnderecoEntrega().getCidade().getEstado().setNome(cidade.getEstado().getNome());
+
+	}
+	
+	private void validarItens(Pedido pedido) {
+		for (ItemPedido itemPedido : pedido.getItens()) {
+			cadastroProdutoService.buscarOuFalhar(pedido.getRestaurante().getId(),itemPedido.getProduto().getId());
+			Produto produto = cadastroProdutoService.buscarOuFalhar(itemPedido.getProduto().getId());
+			
+			double total = itemPedido.getQuantidade()*produto.getPreco().doubleValue();
+			itemPedido.setPrecoUnitario(produto.getPreco());
+			itemPedido.setPrecoTotal(new BigDecimal(total));
+		}
 	}
 	
 }
