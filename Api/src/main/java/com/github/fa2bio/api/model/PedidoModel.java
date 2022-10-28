ad570db32a311c541d34736cd0a58dfa781f3af1
Fábio <fa2bio.s0@gmail.com>
2022-10-28 18:13:50 -0300

    Update PedidoModel.java

--- Api/src/main/java/com/github/fa2bio/api/model/PedidoModel.java
+++ Api/src/main/java/com/github/fa2bio/api/model/PedidoModel.java
@@ -11,18 +11,18 @@ import lombok.Setter;
 @Setter
 public class PedidoModel {
 	
-	private Long id;
+	private Long id; 
 	private BigDecimal subtotal;
 	private BigDecimal taxaFrete;
 	private BigDecimal valorTotal;
 	private String status;
-	private OffsetDateTime dataCriacao;
-	private OffsetDateTime dataConfirmacao;
-	private OffsetDateTime dataEntrega;
-	private OffsetDateTime dataCancelamento;
-	private RestauranteResumoModel restaurante;
-	private UsuarioModel cliente;
-	private FormaPagamentoModel formaPagamento;
-	private EnderecoModel enderecoEntrega;
-	private List<ItemPedidoModel> itens;
+	private OffsetDateTime dataCriacao; 
+	private OffsetDateTime dataConfirmacao; 
+	private OffsetDateTime dataEntrega; 
+	private OffsetDateTime dataCancelamento; 
+	private RestauranteResumoModel restaurante; 
+	private UsuarioModel cliente; 
+	private FormaPagamentoModel formaPagamento; 
+	private EnderecoModel enderecoEntrega; 
+	private List<ItemPedidoModel> itens; 
 }
