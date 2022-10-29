c61b66f47f6792e9dd0c3729c96a3f6a8a3b1d57
Fábio <fa2bio.s0@gmail.com>
2022-10-29 14:02:53 -0300

    Update afterMigrate.sql

--- Api/src/main/resources/db/testdata/afterMigrate.sql
+++ Api/src/main/resources/db/testdata/afterMigrate.sql
@@ -91,10 +91,10 @@ insert into usuario_grupo (usuario_id, grupo_id) values (1,1), (2,2), (3,3), (4,
 
 insert into restaurante_usuario (restaurante_id, usuario_id) values (1,1), (2,2), (3,3), (4,4), (5,2), (6,3),(1,4);
 
-insert into pedido (id, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep, 
+insert into pedido (id, codigo, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep, 
                     endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro,
 	                status, data_criacao, subtotal, taxa_frete, valor_total)
-values (1, 1, 1, 1, 1, '38400-000', 'Rua Floriano Peixoto', '500', 'Apto 801', 'Brasil',
+values (1, '5a76ddc6-fc45-468d-8b8d-bc8bef3462b4', 1, 1, 1, 1, '38400-000', 'Rua Floriano Peixoto', '500', 'Apto 801', 'Brasil',
         'CRIADO', utc_timestamp, 298.90, 10, 308.90);
 
 insert into item_pedido (id, pedido_id, produto_id, quantidade, preco_unitario, preco_total, observacao)
@@ -103,10 +103,10 @@ values (1, 1, 1, 1, 28.9, 28.9, null);
 insert into item_pedido (id, pedido_id, produto_id, quantidade, preco_unitario, preco_total, observacao)
 values (2, 1, 2, 2, 25.9, 51.8, 'Com mais queijo, por favor');
 
-insert into pedido (id, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep, 
+insert into pedido (id, codigo, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep, 
                     endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro,
 	                status, data_criacao, subtotal, taxa_frete, valor_total)
-values (2, 4, 1, 2, 1, '38400-111', 'Rua Acre', '300', 'Casa 2', 'Centro',
+values (2, 'c7c4b411-de31-405c-9294-1a7e669b8398', 4, 1, 2, 1, '38400-111', 'Rua Acre', '300', 'Casa 2', 'Centro',
         'CRIADO', utc_timestamp, 79, 0, 79);
         
 insert into item_pedido (id, pedido_id, produto_id, quantidade, preco_unitario, preco_total, observacao)
