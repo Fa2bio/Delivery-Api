218b41c47c1d56b90b6b74bd958cf8b430aa7b63
Fábio <fa2bio.s0@gmail.com>
2022-11-01 13:53:42 -0300

    Update CozinhaInputDisassembler.java

--- Api/src/main/java/com/github/fa2bio/api/assembler/CozinhaInputDisassembler.java
+++ Api/src/main/java/com/github/fa2bio/api/assembler/CozinhaInputDisassembler.java
@@ -1,14 +1,11 @@
 package com.github.fa2bio.api.assembler;
 
-import java.util.ArrayList;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.github.fa2bio.api.model.input.CozinhaInput;
 import com.github.fa2bio.domain.model.Cozinha;
-import com.github.fa2bio.domain.model.Restaurante;
 
 @Component
 public class CozinhaInputDisassembler {
@@ -21,9 +18,6 @@ public class CozinhaInputDisassembler {
 	}
 	
 	public void copyToDomainObject(CozinhaInput cozinhaInput, Cozinha cozinha) {
-		//Para evitar org.hibernate.HibernateException: identifier of an instance 
-		//of com.github.fa2bio.domain.model.Restaurante was altered from 1 to 2
-		cozinha.setRestaurantes(new ArrayList<Restaurante>());
 		modelMapper.map(cozinhaInput, cozinha);
 	}
 }
