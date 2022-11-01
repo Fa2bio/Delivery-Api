b643e4356bdcd773919c3586afea042800bc4f16
Fábio <fa2bio.s0@gmail.com>
2022-11-01 14:45:20 -0300

    Update ModelMapperConfig.java

--- Api/src/main/java/com/github/fa2bio/core/modelmapper/ModelMapperConfig.java
+++ Api/src/main/java/com/github/fa2bio/core/modelmapper/ModelMapperConfig.java
@@ -4,7 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.github.fa2bio.api.model.EnderecoModel;
 import com.github.fa2bio.api.model.input.ItemPedidoInput;
+import com.github.fa2bio.domain.model.Endereco;
 import com.github.fa2bio.domain.model.ItemPedido;
 
 @Configuration
@@ -16,6 +18,13 @@ public class ModelMapperConfig {
 		
 		var skipItemPedidoId = modelMapper.createTypeMap(ItemPedidoInput.class, ItemPedido.class);
 		skipItemPedidoId.addMappings(mapper -> mapper.skip(ItemPedido::setId));
+		
+		var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(
+				Endereco.class, EnderecoModel.class);
+		
+		enderecoToEnderecoModelTypeMap.<String>addMapping(
+				enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+				(enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
 		
 		return modelMapper;
 	}
