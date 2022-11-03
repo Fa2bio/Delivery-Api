081dfdb1635d60b4bf4d05d96c82b988ce4ed6b6
Fábio <fa2bio.s0@gmail.com>
2022-11-03 09:35:25 -0300

    Update PedidoRepository.java

--- Api/src/main/java/com/github/fa2bio/domain/repository/PedidoRepository.java
+++ Api/src/main/java/com/github/fa2bio/domain/repository/PedidoRepository.java
@@ -3,11 +3,13 @@ package com.github.fa2bio.domain.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import com.github.fa2bio.domain.model.Pedido;
 
-public interface PedidoRepository extends CustomJpaRepository<Pedido, Long>{
+public interface PedidoRepository extends CustomJpaRepository<Pedido, Long>,
+		JpaSpecificationExecutor<Pedido>{
 	
 	@Query("from Pedido p join fetch p.cliente join fetch p.restaurante r join fetch r.cozinha")
 	List<Pedido> findAll();
