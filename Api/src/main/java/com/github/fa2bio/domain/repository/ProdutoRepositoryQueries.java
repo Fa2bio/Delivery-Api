71e3c2b2cb85e4dd3d67d92baf20404448a6fa9e
Fábio <fa2bio.s0@gmail.com>
2022-11-21 10:15:33 -0300

    Cria ProdutoRepositoryQueries

---
+++ Api/src/main/java/com/github/fa2bio/domain/repository/ProdutoRepositoryQueries.java
@@ -0,0 +1,7 @@
+package com.github.fa2bio.domain.repository;
+
+import com.github.fa2bio.domain.model.FotoProduto;
+
+public interface ProdutoRepositoryQueries {
+	FotoProduto save(FotoProduto foto);
+}
