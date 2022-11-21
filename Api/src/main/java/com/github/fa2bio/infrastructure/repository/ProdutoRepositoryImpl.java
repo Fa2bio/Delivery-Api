8e3bb5f7b9ac041bd02e842f7641e192ea47fd49
Fábio <fa2bio.s0@gmail.com>
2022-11-21 10:15:56 -0300

    Cria ProdutoRepositoryImpl

---
+++ Api/src/main/java/com/github/fa2bio/infrastructure/repository/ProdutoRepositoryImpl.java
@@ -0,0 +1,24 @@
+package com.github.fa2bio.infrastructure.repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.fa2bio.domain.model.FotoProduto;
+import com.github.fa2bio.domain.repository.ProdutoRepositoryQueries;
+
+@Repository
+public class ProdutoRepositoryImpl implements ProdutoRepositoryQueries{
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	@Transactional
+	public FotoProduto save(FotoProduto foto) {
+		return manager.merge(foto);
+	}
+
+}
