bd511113735a2c7ab16041e33ae7e678973c6161
Fábio <fa2bio.s0@gmail.com>
2022-11-21 10:13:21 -0300

    Update ProdutoRepository

--- Api/src/main/java/com/github/fa2bio/domain/repository/ProdutoRepository.java
+++ Api/src/main/java/com/github/fa2bio/domain/repository/ProdutoRepository.java
@@ -11,7 +11,7 @@ import com.github.fa2bio.domain.model.Produto;
 import com.github.fa2bio.domain.model.Restaurante;
 
 @Repository
-public interface ProdutoRepository extends CustomJpaRepository<Produto, Long>{
+public interface ProdutoRepository extends CustomJpaRepository<Produto, Long>, ProdutoRepositoryQueries{
 
 	@Query("from Produto where restaurante.id = :restaurante and id = :produto")
 	Optional<Produto> findById(@Param("restaurante") Long restauranteId, 
