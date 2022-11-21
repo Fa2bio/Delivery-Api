e46f9cfa74c44898a57f9c8a9845806e88c07eaa
Fábio <fa2bio.s0@gmail.com>
2022-11-21 10:13:48 -0300

    Cria FotoProdutoModel

---
+++ Api/src/main/java/com/github/fa2bio/api/model/FotoProdutoModel.java
@@ -0,0 +1,13 @@
+package com.github.fa2bio.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FotoProdutoModel {
+	private String nomeArquivo;
+	private String descricao;
+	private String contentType;
+	private Long tamanho;
+}
