0b06cdcdbe16ce11a6dc3f050906fa391dcfba25
Fábio <fa2bio.s0@gmail.com>
2022-11-21 10:13:44 -0300

    Cria FotoProdutoModelAssembler

---
+++ Api/src/main/java/com/github/fa2bio/api/assembler/FotoProdutoModelAssembler.java
@@ -0,0 +1,19 @@
+package com.github.fa2bio.api.assembler;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.github.fa2bio.api.model.FotoProdutoModel;
+import com.github.fa2bio.domain.model.FotoProduto;
+
+@Component
+public class FotoProdutoModelAssembler {
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	public FotoProdutoModel toModel(FotoProduto foto) {
+		return modelMapper.map(foto, FotoProdutoModel.class);
+	}
+}
