3861f09f964a753f728de53437193e433df3b7c5
Fábio <fa2bio.s0@gmail.com>
2022-11-26 11:57:42 -0300

    Update OrderStatusController.java

--- Api/src/main/java/com/github/fa2bio/api/controller/OrderStatusController.java
+++ Api/src/main/java/com/github/fa2bio/api/controller/OrderStatusController.java
@@ -8,27 +8,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.fa2bio.api.swaggeropenapi.controller.OrderStatusControllerSwagger;
 import com.github.fa2bio.domain.service.OrderStatusService;
 
 @RestController
 @RequestMapping(value = "/orders/{orderCode}")
-public class OrderStatusController {
+public class OrderStatusController implements OrderStatusControllerSwagger{
 	
 	@Autowired
 	private OrderStatusService orderStatusService;
 	
+	@Override
 	@PutMapping("/confirm")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void confirm(@PathVariable String orderCode) {
 		orderStatusService.confirmar(orderCode);
 	}
 	
+	@Override
 	@PutMapping("/cancellation")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void cancellation(@PathVariable String orderCode) {
 		orderStatusService.cancelar(orderCode);
 	}
 	
+	@Override
 	@PutMapping("/delivered")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delivered(@PathVariable String orderCode) {
