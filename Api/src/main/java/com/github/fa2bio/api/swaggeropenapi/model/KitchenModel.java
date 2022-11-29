80d54ad26030a82a3b066cc4583fcb55eb96d713
Fábio <fa2bio.s0@gmail.com>
2022-11-29 20:02:53 -0300

    Revert "Delete model folder in swaggeropenapi"
    This reverts commit ab8f3eac11465c9466b6c329988edce5babd1fca.

---
+++ Api/src/main/java/com/github/fa2bio/api/swaggeropenapi/model/KitchenModel.java
@@ -0,0 +1,8 @@
+package com.github.fa2bio.api.swaggeropenapi.model;
+
+import io.swagger.annotations.ApiModel;
+
+@ApiModel("KitchenModel")
+public class KitchenModel extends PagedModelOpenApi<KitchenModel>{
+
+}
