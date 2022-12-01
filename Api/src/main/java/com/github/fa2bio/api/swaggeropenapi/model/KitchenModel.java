4db7c94413c32209c708fbe7b2717e63f6fb0adc
Fábio <fa2bio.s0@gmail.com>
2022-12-01 09:44:46 -0300

    Delete KitchenModel.java

--- Api/src/main/java/com/github/fa2bio/api/swaggeropenapi/model/KitchenModel.java
+++
@@ -1,8 +0,0 @@
-package com.github.fa2bio.api.swaggeropenapi.model;
-
-import io.swagger.annotations.ApiModel;
-
-@ApiModel("KitchenModel")
-public class KitchenModel extends PagedModelOpenApi<KitchenModel>{
-
-}
