ff80f9bb31d03ef5a168d4be300361bea3049930
Fábio <fa2bio.s0@gmail.com>
2022-12-02 22:07:22 -0300

    Update ProblemType.java

--- Api/src/main/java/com/github/fa2bio/api/exceptionhandler/ProblemType.java
+++ Api/src/main/java/com/github/fa2bio/api/exceptionhandler/ProblemType.java
@@ -5,13 +5,13 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
-	DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos"),
-	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
-	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
-	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
-	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
-	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
-	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
+	INVALID_DATA("/invalid-data", "Invalid data"),
+	SYSTEM_ERROR("/system-error", "System error"),
+	INVALID_PARAMETER("/invalid-parameter", "Invalid parameter"),
+	MESSAGE_INCOMPREENSIBLE("/message-incompreesible", "Message incompreesible"),
+	RESOURCE_NOT_FUND("/resource-not-found", "Resource not found"),
+	ENTITY_IN_USE("/entity-in-use", "Entity in use"),
+	BUSINESS_ERROR("/business-error", "Business error");
 	
 	private String title;
 	private String uri;
