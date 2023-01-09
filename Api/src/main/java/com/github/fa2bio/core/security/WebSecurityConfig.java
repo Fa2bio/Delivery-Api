80614babb83f3b74a8132fb02ce5392a3e8afd97
Fábio <fa2bio.s0@gmail.com>
2023-01-09 19:24:18 -0300

    Update WebSecurityConfig.java

--- Api/src/main/java/com/github/fa2bio/core/security/WebSecurityConfig.java
+++ Api/src/main/java/com/github/fa2bio/core/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -36,6 +37,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 				.password(passwordEncoder().encode("123"))
 				.roles("ADMIN");
 	}
+	
+	@Override
+    	public void configure(WebSecurity web) throws Exception {
+        	web.ignoring().antMatchers("/v2/api-docs",
+                                   "/configuration/ui",
+                                   "/swagger-resources/**",
+                                   "/configuration/security",
+                                   "/swagger-ui/**",
+                                   "/swagger-ui.html",
+                                   "/webjars/**");
+    }
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
